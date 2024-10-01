de01121e5e99f32b0c7d0e47958d457c2ea245f6
sidharth2189 <sidharth2189@gmail.com>
2024-10-02 01:58:53 +0200

    Remove sample shell script for turtlebot

--- shell_scripts/test_slam_turtlebot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
-sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"

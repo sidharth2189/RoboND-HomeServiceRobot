75913ceaa9f7f9cba79f0a04b793142bd90e4af2
sidharth2189 <sidharth2189@gmail.com>
2024-10-02 18:17:35 -0700

    Reuse saved rviz config instead of view_navigation.launch

--- shell_scripts/pick_objects.sh
+++ shell_scripts/pick_objects.sh
@@ -3,6 +3,4 @@ xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setu
 sleep 3
 xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch my_robot amcl.launch" &
 sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 3
 xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch pick_objects pick_objects.launch"
--- shell_scripts/test_navigation.sh
+++ shell_scripts/test_navigation.sh
@@ -2,5 +2,3 @@
 xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch my_robot world.launch " &
 sleep 3
 xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch my_robot amcl.launch" &
-sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"

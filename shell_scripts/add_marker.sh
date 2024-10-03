5b28713b4d5c3e6421731c4fbb85b2d131263882
sidharth2189 <sidharth2189@gmail.com>
2024-10-03 02:50:31 -0700

    Launch test node with add_marker_test

--- shell_scripts/add_marker.sh
+++ shell_scripts/add_marker.sh
@@ -3,4 +3,4 @@ xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setu
 sleep 3
 xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch my_robot amcl.launch" &
 sleep 3
-xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch add_markers add_markers.launch"
+xterm -e " source /opt/ros/kinetic/setup.bash; cd ~/catkin_ws; source devel/setup.bash; roslaunch add_markers add_markers_test.launch"
